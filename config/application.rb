0728a6a709a04570b6d61e8878e6855da5c28ea0
Jasmin <98263758+nachtjasmin@users.noreply.github.com>
2023-07-06 21:30:13 +0200

    Merge upstream security fixes of v4.0.5 (#1316)
    It's already running on our instance (queer.group) and working fine.
    
    Manually reviewed the changes, hadn't found anything that could break
    hometown-specific code.
    And to update our instance, I also just followed the [steps on the
    release](https://github.com/mastodon/mastodon/releases/tag/v4.0.5) aka
    `bundle install && yarn install` followed by a restart of all processes.
    
    ---------
    
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>
    Co-authored-by: Daniel M Brasil <danielmbrasil@protonmail.com>
    Co-authored-by: Emelia Smith <ThisIsMissEm@users.noreply.github.com>
    Co-authored-by: Vyr Cossont <VyrCossont@users.noreply.github.com>
    Co-authored-by: Renaud Chaput <renchap@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ require_relative '../lib/paperclip/url_generator_extensions'
 require_relative '../lib/paperclip/attachment_extensions'
 require_relative '../lib/paperclip/lazy_thumbnail'
 require_relative '../lib/paperclip/gif_transcoder'
+require_relative '../lib/paperclip/media_type_spoof_detector_extensions'
 require_relative '../lib/paperclip/transcoder'
 require_relative '../lib/paperclip/type_corrector'
 require_relative '../lib/paperclip/response_with_limit_adapter'
@@ -159,6 +160,10 @@ module Mastodon
         :en
       end
     end
+
+    config.public_file_server.headers = {
+      'X-Content-Type-Options' => 'nosniff',
+    }
 
     # config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
     # config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
